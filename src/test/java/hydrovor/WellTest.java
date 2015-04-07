ca003e803f1cbece495b3c5e442ef97465d01f11
w <w>
2015-04-07 21:41:52 +0200

    WellAdded

--- src/test/java/hydrovor/WellTest.java
+++ src/test/java/hydrovor/WellTest.java
@@ -1,5 +1,43 @@
 package hydrovor;
 
-public class WellTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+public class WellTest {
+    private Well well;
+    
+    @Test
+    public void WellIsEmpty()
+    {
+        //        Given
+            well = new Well(0);
+        //        When
+        int result = well.getWater(1);
+
+        //        Then
+        Assert.assertEquals(0, result);
+    }
+
+    @Test
+    public void IsWater()
+    {
+        //        Given
+            well = new Well(10);
+        //        When
+        int result = well.getWater(10);
+        //        Then
+        Assert.assertEquals(10, result);
+    }
+        @Test
+    public void IsEmptying()
+    {
+        //        Given
+            well = new Well(10);
+        //        When
+        well.getWater(10);
+        int result = well.getWater(10);
+        //        Then
+        Assert.assertEquals(0, result);
+    }
 }
